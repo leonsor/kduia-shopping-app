70b42cb68c43623ee282900a49e03cb66882124b
leonsor <34835973+leonsor@users.noreply.github.com>
2023-05-28 16:29:40 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +9,25 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+      <div className='container'>
+        <h1 className='mt-3'> Shopping App </h1>
+        <div className= 'row mt-3'>
+          <div className='col-sm'>
+            <CartValue />
+          </div>
+          <div className='col-sm'>
+            <Location />
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <ExpenseList />
+        </div>
+        <div className='row mt-3'>
+          <ItemSelected />
+        </div>
+      </div>
+    </AppProvider>
   );
 }
 
